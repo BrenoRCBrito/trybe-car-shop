1795e562e10d57886cd9e85fe442bae23eabd41c
BrenoRCBrito <brenorcbrito@gmail.com>
2022-05-25 19:16:08 -0300

    fix[ModelInterface]: add DeleteResult interface to delete method res :rotating_light:

--- src/interfaces/ModelInterface.ts
+++ src/interfaces/ModelInterface.ts
@@ -1,6 +1,8 @@
+import { DeleteResult } from 'mongodb';
+
 export interface Model<T> {
   create(obj: T): Promise<T>;
   read(): Promise<T[]>;
   readOne(id_: string): Promise<T | null>;
-  delete(id_: string): Promise<T | null>;
+  delete(id_: string): Promise<T | DeleteResult | null>;
 }

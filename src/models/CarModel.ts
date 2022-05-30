9454e66aa8129e32f3640df52a922f01d50e886e
BrenoRCBrito <brenorcbrito@gmail.com>
2022-05-30 14:53:43 -0300

    feat[model]: add document keys to schema :sparkles:

--- src/models/CarModel.ts
+++ src/models/CarModel.ts
@@ -4,7 +4,17 @@ import MongoModel from './MongoModel';
 
 interface CarDocument extends Car, Document {}
 
-const carSchema = new Schema<CarDocument>();
+const carSchema = new Schema<CarDocument>(
+  {
+    buyValue: Number,
+    color: String,
+    doorsQty: Number,
+    model: String,
+    seatsQty: Number,
+    year: Number,
+  },
+  { versionKey: false },
+);
 
 export default class CarModel extends MongoModel<Car> {
   constructor(model = createModel('Cars', carSchema)) {

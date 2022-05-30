59661fd6bbfe97ecfc94bcc226141875d1cea519
BrenoRCBrito <brenorcbrito@gmail.com>
2022-05-30 14:52:46 -0300

    feat[interface]: add update method :sparkles:

--- src/interfaces/ModelInterface.ts
+++ src/interfaces/ModelInterface.ts
@@ -4,5 +4,6 @@ export interface Model<T> {
   create(obj: T): Promise<T>;
   read(): Promise<T[]>;
   readOne(id_: string): Promise<T | null>;
+  update(id: string, obj: T): Promise<T | null>;
   delete(id_: string): Promise<T | DeleteResult | null>;
 }

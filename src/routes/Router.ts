581dad303ec25834ae7789bae1c1077204537ccf
BrenoRCBrito <brenorcbrito@gmail.com>
2022-05-30 14:57:57 -0300

    feat[router]: add put and delete endpoints :sparkles:

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -11,8 +11,9 @@ class Router<T> {
   public addRoute(controller: Controller<T>, route: string = controller.route) {
     this.router.get(route, controller.read);
     this.router.get(`${route}/:id`, controller.readOne);
-    console.log(route);
     this.router.post(route, controller.create);
+    this.router.put(`${route}/:id`, controller.update);
+    this.router.delete(`${route}/:id`, controller.delete);
   }
 }
 

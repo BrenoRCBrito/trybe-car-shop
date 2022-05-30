fc448cb162bc20feb25e628c2328128e233d1e4c
BrenoRCBrito <brenorcbrito@gmail.com>
2022-05-30 14:56:13 -0300

    feat[controllers]: add update and delete methods, also add error message to readOne method :sparkles:

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -40,9 +40,49 @@ export default class CarController extends Controller<Car> {
   ): Promise<typeof res> => {
     const { id } = req.params;
     try {
+      if (id.length !== 24) {
+        return res.status(400).send({ error: this.errors.irregularId });
+      }
       const car = await this.service.readOne(id);
       return car
         ? res.json(car)
+        : res.status(404).json({ error: this.errors.notFound });
+    } catch (error) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+
+  update = async (
+    req: RequestWithBody<Car>,
+    res: Response<Car | ResponseError>,
+  ): Promise<typeof res> => {
+    const { id } = req.params;
+    const { body } = req;
+    try {
+      if (id.length !== 24) {
+        return res.status(400).send({ error: this.errors.irregularId });
+      }
+      const car = await this.service.update(id, body);
+      if (!car) return res.status(404).json({ error: 'Object not found' });
+      if ('error' in car) return res.status(400).json(car);
+      return res.status(200).json(car);
+    } catch (err) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+
+  delete = async (
+    req: Request<{ id: string }>,
+    res: Response<Car | ResponseError>,
+  ): Promise<typeof res> => {
+    const { id } = req.params;
+    try {
+      if (id.length !== 24) {
+        return res.status(400).send({ error: this.errors.irregularId });
+      }
+      const car = await this.service.readOne(id);
+      return car
+        ? res.status(204).json(car)
         : res.status(404).json({ error: this.errors.notFound });
     } catch (error) {
       return res.status(500).json({ error: this.errors.internal });
